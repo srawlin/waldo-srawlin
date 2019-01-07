94e1c39ad649815e81f554b882307a86ce58b219
Steven Rawlinson <srawlin@gmail.com>
2019-01-07 10:52:30 -0500

    Improved error handling for corrupt images

--- waldo_worker/tasks.py
+++ waldo_worker/tasks.py
@@ -9,19 +9,15 @@ def run_match(image1_filename, image2_filename):
     # Open both files and check
     try:
         image1 = WaldoImage(image1_filename)
-    except IOError:
-        print("The file does not exist")
-    except ValueError:
-        print("Invalid file format")
+    except (IOError, ValueError):
+        return (image1_filename, None)
     except Exception:
         print("Unexpected error occured")
 
     try:
         image2 = WaldoImage(image2_filename)
-    except IOError:
-        print("The file does not exist")
-    except ValueError:
-        print("Invalid file format")
+    except (IOError, ValueError):
+        return (image2_filename, None)
     except Exception:
         print("Unexpected error occured")
 
