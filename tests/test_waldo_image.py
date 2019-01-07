7be5e7afe2c7e6bfa55b6ae117898056f090c47f
Steven Rawlinson <srawlin@gmail.com>
2019-01-06 23:20:14 -0500

    Added test for corrupt image files

--- tests/test_waldo_image.py
+++ tests/test_waldo_image.py
@@ -21,11 +21,14 @@ class TestWaldoImage(unittest.TestCase):
         # cropped image correctly found
         self.assertEqual(self.image1.match(self.image1_cropped), (997, 214))
 
-
     def test_match_below_threshold(self):
         # false positive with threshold of 0.73
         self.assertEqual(self.image1.match(self.image_no_match_small, threshold=0.7), (1224, 436))
         self.assertEqual(self.image1.match(self.image_no_match_small, threshold=0.8), None)
+
+    def test_corrupt_image_file(self):
+        with self.assertRaises(ValueError):
+            self.image_corrupt_jpg = WaldoImage('test-images/image5.jpg')
 
 if __name__ == '__main__':
     unittest.main()
